6967e5f01a43f113e724e54208a025cc7bc5b398
Bekarys71 <Beybarys.imashev@mail.ru>
2024-09-01 15:21:20 +0600

    Add files via upload

---
+++ 1программа.py
@@ -0,0 +1,11 @@
+# "1st Programm"
+print(9 ** 0.5 * 5)
+# "2nd Programm"
+print(99.9 > 99.8) , (1000 != 1000.1)
+# "3rd Programm"
+print((2 * 2 + 2)  == print(2 * (2 +2)))
+# "4th Programm"
+x = float("123,456")
+y = (x * 10)
+print(int( y ))
+
